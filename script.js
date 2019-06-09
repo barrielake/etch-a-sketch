952b4039c3dffac9d891c11c47582361fc5ada0e
Barrie Lake <lake.barrie@gmail.com>
2019-06-09 20:11:42 +0200

    Add reset button allowing user to pick number of cells per side

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function createGrid(number){
     for (let i = 0; i < number*number; i++){
         let cell = document.createElement("div");
         cell.addEventListener("mouseover", function(e) {
-            let targetCell = e.target;
-            changeColor(targetCell);
+            changeColor(e.target);
         });
         container.appendChild(cell);
     }
@@ -16,4 +15,17 @@ createGrid(16);
 
 function changeColor(targetCell){
     targetCell.setAttribute("style", "background-color: black");
+}
+
+const resetButton = document.querySelector("#reset-button");
+console.log(resetButton);
+resetButton.addEventListener("click", resetBoard);
+
+function resetBoard(){
+    //clear the container
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    let number = prompt("How many cells per side?");
+    createGrid(number);
 }
