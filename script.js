cd663cb0d3dd856c9a03c7764fe09b2ef3122f47
Barrie Lake <lake.barrie@gmail.com>
2019-06-09 23:04:49 +0200

    Add random color functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#grid-container");
+let colorMode = "default";
 
 function createGrid(number){
     container.setAttribute("style", `grid-template: repeat(${number}, 1fr) / repeat(${number}, 1fr);`)
@@ -15,11 +16,27 @@ function createGrid(number){
 createGrid(16);
 
 function changeColor(targetCell){
-    targetCell.setAttribute("style", "background-color: #ea5c5a");
+    switch(colorMode){
+        case "default":
+            targetCell.setAttribute("style", "background-color: #ea5c5a");
+            break;
+        case "random":
+            targetCell.setAttribute("style", `background-color: ${getRandomColor()};`);
+    }
+}
+
+function getRandomColor(){
+    let r = getRandomInt(255);
+    let g = getRandomInt(255);
+    let b = getRandomInt(255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function getRandomInt(maxNumber){
+    return Math.floor(Math.random() * (maxNumber + 1));
 }
 
 const resetButton = document.querySelector("#reset-button");
-console.log(resetButton);
 resetButton.addEventListener("click", resetBoard);
 
 function resetBoard(){
@@ -29,4 +46,10 @@ function resetBoard(){
     }
     let number = prompt("How many cells per side?");
     createGrid(number);
+    colorMode = "default";
 }
+
+const randomButton = document.querySelector("#random-button");
+randomButton.addEventListener("click", function(){
+    colorMode = "random";
+});
