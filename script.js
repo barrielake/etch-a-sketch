517cc6e68450e5c6048886b0d479d8929247506d
Barrie Lake <lake.barrie@gmail.com>
2019-06-09 19:49:12 +0200

    Change cell color to black on mouseover event

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ function createGrid(number){
     container.setAttribute("style", `grid-template: repeat(${number}, 1fr) / repeat(${number}, 1fr);`)
     for (let i = 0; i < number*number; i++){
         let cell = document.createElement("div");
-        cell.classList.add("grid-item");
+        cell.addEventListener("mouseover", function(e) {
+            let targetCell = e.target;
+            changeColor(targetCell);
+        });
         container.appendChild(cell);
     }
 }
 
 createGrid(16);
+
+function changeColor(targetCell){
+    targetCell.setAttribute("style", "background-color: black");
+}
